3f7bea0860548503b0aee6bb9f2c7ac6d95650c4
Fincarson <vincentjeffersonharjo@gmail.com>
2025-08-10 01:51:59 +0800

    Added Solution
    Added Solution for "Karpet Ribet"

---
+++ C/13339_Karpet_Ribet/Solution_1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int array[3100][3100];
+void karpetRibet(int x, int y, int n) {
+    if (n == 0)return;
+    int h = n / 2;
+
+    for (int i = 0;i < n;i++) {
+        for (int j = 0;j < n;j++) {
+            array[x + i][y + j] = 1;
+        }
+    }
+    karpetRibet(x - h, y - h, h);
+    karpetRibet(x - h, y + n, h);
+    karpetRibet(x + n, y - h, h);
+    karpetRibet(x + n, y + n, h);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    n--;
+    int d = 1 << n;             // 2 to the power of n
+    int span = (d - 1) * 2 + d;     // span of carpet
+
+    for (int i = 0;i < span;i++) {
+        for (int j = 0;j < span;j++) {
+            array[i][j] = 0;
+        }
+    }
+
+    karpetRibet(d - 1, d - 1, d);
+
+    for (int i = 0;i < span;i++) {
+        for (int j = 0;j < span;j++) {
+            if (array[i][j] == 1) printf("*");
+            else printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
